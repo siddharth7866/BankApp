bab4d3cc88855602d0872b2b846f85044c2e67c8
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2023-10-18 11:07:42 +0530

    fix

---
+++ p.js
@@ -0,0 +1,40 @@
+// var sid=[100,200,-300,45,120,-1100]
+// for (let [a,i] of sid.entries()) {
+//   if (i<0) {
+//     console.log(` Transaction:${a+1}, ${Math.abs(i)}  rupees debit from account `)
+//   }else{
+//     console.log(` Transaction:${a+1}, ${i} rupees depoited in account`)
+//   }
+// }
+
+// console.log("For Each--------------------------")
+// sid.forEach(function (i) {
+//     if (i<0) {
+//         console.log(`${Math.abs(i)}  rupees debit from account `)
+//       }else{
+//         console.log(`${i} rupees depoited in account`)
+//       }
+    
+// })
+//  import prompt from 'prompt';
+
+// "const prompt = require('prompt');"
+// var MarkHeight=prompt("Enter height of Mark")
+// var MarkWeight=prompt("Enter Weight of Mark")
+
+// var MarkHeight=prompt("Enter height of Mark")
+// var MarkWeight=prompt("Enter Weight of Mark")
+// const BMIMark=MarkWeight/(MarkHeight*MarkHeight)
+// alert(`The BMI of Mark is ${BMIMark}`)
+// var JohnHeight=prompt("Enter height of Mark")
+// var JohnWeight=prompt("Enter Weight of Mark")
+// const BMIJohn=JohnWeight/(JohnHeight*JohnHeight)
+// alert(`The BMI of John is ${BMIJohn}`)
+// if (condition) {
+    
+// }
+
+
+
+
+
